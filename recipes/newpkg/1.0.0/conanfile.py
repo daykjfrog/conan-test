7af8ab9736ae1f8c87a567a84fa63b700577e6a0
daykjfrog <91162273+daykjfrog@users.noreply.github.com>
2023-06-22 14:46:04 -0700

    Update conanfile.py

--- recipes/newpkg/1.0.0/conanfile.py
+++ recipes/newpkg/1.0.0/conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 
 class NewpkgConan(ConanFile):
-    name = "rgt"
+    name = "newpkg"
     version = "1.0.0"
     # requires = "openssl/1.1.1o@"
     # Optional metadata
